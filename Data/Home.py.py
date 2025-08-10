bebf82dfc2511f401c1e6dcbdf5434902787fce3
Olaf Bulas <o.bulas@outlook.de>
2025-08-10 18:59:46 +0200

    fixes to debug code

--- Data/Home.py.py
+++ Data/Home.py.py
@@ -8,8 +8,8 @@ import Write
 from datetime import datetime, timedelta
 
 #debug:
-  tables = conn.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()
-    print(tables)
+tables = conn.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()
+print(tables)
 
 # Path to your database file in the repo
 db_path = os.path.join(os.path.dirname(__file__), "library.db")
