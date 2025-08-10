2b7612c0b8ab597799f1b3d3547cddd9b0d9f49f
Olaf Bulas <o.bulas@outlook.de>
2025-08-10 18:15:29 +0200

    Relative Path Setup
    Changed to a relative path setup

--- Data/library_connection.py
+++ Data/library_connection.py
@@ -1,4 +1,7 @@
 from sqlalchemy import create_engine
 
-connection_string = "sqlite:///library.db"
-engine = create_engine(connection_string)
+# Determine location relative to this file
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(PROJECT_ROOT, "library.db")
+
+engine = create_engine(f"sqlite:///{DB_PATH}", echo=False)
